68541912f2edd2ca06350f9c6cad8a369e31fc83
juharyimami <juharyimami2011@gmail.com>
2023-05-13 23:12:41 +0300

    forms added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'users/index'
-  # get 'users/show'
-  # below is the code I solved this issues of previous review.
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
+  resources :posts, only: [:new, :create]
 end
