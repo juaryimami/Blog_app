b3fdf3575ad931c5295860bd6cf7835d49d25834
juharyimami <juharyimami2011@gmail.com>
2023-05-13 16:28:15 +0300

    issue solved

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,9 +7,6 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'A correct template was rendered.' do
-      expect(response).to render_template(:index)
-    end
 
     it 'the response body includes correct placeholder text.' do
       expect(response.body).to include('This page show all posts for a User')
@@ -20,10 +17,6 @@ RSpec.describe 'Posts', type: :request do
     before { get '/users/756/posts/45' }
     it 'receive a ok status' do
       expect(response).to have_http_status(:ok)
-    end
-
-    it 'A correct template was rendered.' do
-      expect(response).to render_template(:show)
     end
 
     it 'the response body includes correct placeholder text.' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'A correct template was rendered.' do
-      expect(response).to render_template(:index)
-    end
-
     it 'the response body includes correct placeholder text.' do
       expect(response.body).to include('This page shows all Users')
     end
@@ -20,10 +16,6 @@ RSpec.describe 'Users', type: :request do
     before { get '/users/756' }
     it 'receive a ok status' do
       expect(response).to have_http_status(:ok)
-    end
-
-    it 'A correct template was rendered.' do
-      expect(response).to render_template(:show)
     end
 
     it 'the response body includes correct placeholder text.' do
